2fe6783fe5393640ef942a0bb1de38f0d1a08024
Unknown <david.j.castelli@gmail.com>
2017-05-10 02:24:46 +0200

    courses stable

--- course_exercises/lnd/src/index.js
+++ course_exercises/lnd/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList' // importo l'elemento
+import { SkiDayCount } from './components/SkiDayCount'
 
-// Sicurezza!! aggancio React alla 'window' per evitare che venga restituito 'React is not defined'
 window.React = React
 
 render(
-	<SkiDayList days={
-
-	} />, // qui richiamo il render del contenuto dell'app
-	document.getElementById('react-container')
+<SkiDayCount total={50}
+powder={20}
+backcountry={10}
+goal={100}/>,
+    document.getElementById('react-container')
 )
