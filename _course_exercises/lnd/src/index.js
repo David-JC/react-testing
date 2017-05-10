7cf35ed175aa6bb4a019e4cae25646a587c80db1
David Castelli <david.j.castelli@gmail.com>
2017-05-11 00:40:58 +0200

    update directory and folders

---
+++ _course_exercises/lnd/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { SkiDayList } from './components/SkiDayList'
+
+// richiamo a componente versione 'Stateless Function'
+import { SkiDayCount } from './components/SkiDayCount'
+
+window.React = React
+
+
+render(
+	<SkiDayCount/>,
+	document.getElementById('react-container')
+)
+
+
+/*
+render(
+	<SkiDayList days={
+		[
+			{
+				resort: "Squaw Valley",
+				date: new Date("1/2/2016"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Kirkwood",
+				date: new Date("3/28/2016"),
+				powder: false,
+				backcountry: false
+			},
+			{
+				resort: "Mt. Tallac",
+				date: new Date("4/2/2016"),
+				powder: false,
+				backcountry: true
+			}
+		]
+	}/>,
+    document.getElementById('react-container')
+)
+*/
